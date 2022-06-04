882be58632bd846ecef9d2598b5ece3588a81224
Thanh Tung Hoang <hoangthanhtung1404@gmail.com>
2022-06-04 11:46:41 +0700

    done

--- lib/ui_change_password_page.dart
+++ lib/ui_change_password_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'core/style/style.dart';
+import 'package:nl_web/core/style/style.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 //////////////////branch 2
@@ -29,22 +29,18 @@ class _UiForgotPassState extends State<UiForgotPass> {
         child: SingleChildScrollView(
           scrollDirection: Axis.vertical,
           child: Padding(
-            padding: EdgeInsets.symmetric(vertical: 160.w, horizontal: 0),
+            padding: EdgeInsets.symmetric(vertical: 160, horizontal: 0),
             child: Column(
               children: [
                 Container(
-                  // width: 384,
-                  // height: 700,
-                  width: ScreenUtil().setWidth(384),
-                  // height: ScreenUtil().setHeight(700),
+                  width: 384,
                   decoration: BoxDecoration(
                     color: Colors.white,
                     border: Border.all(),
                     borderRadius: const BorderRadius.all(Radius.circular(8.0)),
                   ),
                   child: Padding(
-                    padding:
-                        EdgeInsets.symmetric(vertical: 40.h, horizontal: 40.w),
+                    padding: EdgeInsets.symmetric(vertical: 40, horizontal: 40),
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
@@ -52,74 +48,70 @@ class _UiForgotPassState extends State<UiForgotPass> {
                           child: Column(
                             crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
-                              // Text("w: $w"),
-                              // Text("h: $h"),
                               setTextTitle(textInput: "Thay đổi mật khẩu"),
+                              const SizedBox(height: 8),
                               setMessage(
                                   textInput:
                                       'Mật khẩu của bạn đã hết hạn. Vui lòng đổi mật khẩu mới để bảo mật thông tin.'),
                             ],
                           ),
                         ),
-                        SizedBox(height: 24.h),
-                        Container(
-                          // width: 304,
-                          // height: 236,
-
-                          // color: Colors.red,
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              inputTextField(
-                                  detail: "Mật khẩu hiện tại",
-                                  controller: TextEditingController()),
-                              SizedBox(height: 16.h),
-                              inputTextField(
-                                  detail: "Mật khẩu mới",
-                                  controller: TextEditingController()),
-                              SizedBox(height: 16.h),
-                              inputTextField(
+                        const SizedBox(height: 24),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            inputTextField(
+                              detail: "Mật khẩu hiện tại",
+                              controller: TextEditingController(),
+                              activeAlert: false,
+                              messageAlert: '',
+                            ),
+                            const SizedBox(height: 16),
+                            inputTextField(
+                              detail: "Mật khẩu mới",
+                              controller: TextEditingController(),
+                              activeAlert: true,
+                              messageAlert:
+                                  'Mật khẩu mới không được trùng với mật khẩu đã được sử dụng trong 5 lần gần nhất.',
+                            ),
+                            const SizedBox(height: 16),
+                            inputTextField(
                                 detail: "Nhập lại mật khẩu mới",
                                 controller: TextEditingController(),
-                              ),
-                              //////////
-                            ],
-                          ),
+                                activeAlert: false,
+                                messageAlert: ''),
+                          ],
                         ),
-                        SizedBox(height: 16.h),
-                        Container(
-                          // width: 304,
-                          // height: 120,
-                          // color: Colors.cyan,
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              checkFormatPassword(
-                                  textCheck: "Tối thiểu 10 ký tự",
-                                  boolCheck: true),
-                              // SizedBox(height: 4),
-                              checkFormatPassword(
-                                  textCheck: "Có ký tự số", boolCheck: true),
-                              // SizedBox(height: 4),
-                              checkFormatPassword(
-                                  textCheck: "Có chữ hoa", boolCheck: true),
-                              // SizedBox(height: 4),
-                              checkFormatPassword(
-                                  textCheck: "Có chữ thường", boolCheck: true),
-                              // SizedBox(height: 4),
-                              checkFormatPassword(
-                                  textCheck: "Có ký tự đặc biệt",
-                                  boolCheck: true),
-                              // SizedBox(height: 4),
-                              checkFormatPassword(
-                                  textCheck:
-                                      "Không trùng với 5 mật khẩu gần đây.",
-                                  boolCheck: true),
-                            ],
-                          ),
+                        const SizedBox(height: 16),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            checkFormatPassword(
+                                textCheck: "Tối thiểu 10 ký tự",
+                                boolCheck: true),
+                            checkFormatPassword(
+                                textCheck: "Có ký tự số", boolCheck: true),
+                            checkFormatPassword(
+                                textCheck: "Có chữ hoa", boolCheck: true),
+                            checkFormatPassword(
+                                textCheck: "Có chữ thường", boolCheck: true),
+                            checkFormatPassword(
+                                textCheck: "Có ký tự đặc biệt",
+                                boolCheck: true),
+                            checkFormatPassword(
+                                textCheck:
+                                    "Không trùng với 5 mật khẩu gần đây.",
+                                boolCheck: true),
+                          ],
                         ),
-                        SizedBox(height: 28.h),
-                        buttonLogin(textTitle: 'Thay Đổi', funcition: null),
+                        const SizedBox(height: 28),
+                        buttonChange(textTitle: 'Thay Đổi', funcition: null),
+                        // const SizedBox(height: 8),
+                        // buttonChangeAndCancel(
+                        //     textTitleChange: 'Thay Đổi',
+                        //     funcitionChange: null,
+                        //     textTitleCancel: 'Hủy',
+                        //     funcitionCancel: null),
                       ],
                     ),
                   ),
@@ -136,8 +128,8 @@ class _UiForgotPassState extends State<UiForgotPass> {
 Widget setTextTitle({required textInput}) {
   return Text(
     textInput,
-    style: TextStyle(
-      fontSize: 20.sp,
+    style: const TextStyle(
+      fontSize: 20,
       fontFamily: mFontRoboto,
       color: AppColor.mCInk900,
     ),
@@ -146,21 +138,20 @@ Widget setTextTitle({required textInput}) {
 
 Widget setMessage({required String textInput}) {
   return DecoratedBox(
-    decoration: BoxDecoration(
+    decoration: const BoxDecoration(
       color: AppColor.mCRed50,
       borderRadius: BorderRadius.all(Radius.circular(8.0)),
     ),
     child: Padding(
-      // padding: EdgeInsets.all(16.0),
-      padding: EdgeInsets.symmetric(vertical: 16.h, horizontal: 16.w),
+      padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 16),
       child: Align(
         alignment: Alignment.center,
         child: Text(
           textInput,
-          style: TextStyle(
+          style: const TextStyle(
             fontFamily: mFontRoboto,
             fontWeight: FontWeight.w400,
-            fontSize: AppDimens.mFontSizeNormal.sp,
+            fontSize: AppDimens.mFontSizeNormal,
             color: Color(0xff98272B),
           ),
         ),
@@ -170,8 +161,10 @@ Widget setMessage({required String textInput}) {
 }
 
 Widget inputTextField(
-    {required String detail, required TextEditingController controller}) {
-  bool _isObscure = true;
+    {required String detail,
+    required TextEditingController controller,
+    required bool activeAlert,
+    required String messageAlert}) {
   return Column(
     crossAxisAlignment: CrossAxisAlignment.start,
     children: [
@@ -181,46 +174,61 @@ Widget inputTextField(
           color: AppColor.mCInk700,
           fontFamily: mFontRoboto,
           fontWeight: FontWeight.w400,
-          fontSize: AppDimens.mFontSizeNormal.sp,
-          // height: AppDimens.mLineHeightXSmall,
+          fontSize: AppDimens.mFontSizeNormal,
         ),
       ),
-      SizedBox(height: 4.h),
+      const SizedBox(height: 4),
       SizedBox(
-        width: 304.w,
-        height: 44.h,
         child: DecoratedBox(
           decoration: BoxDecoration(
-            color: AppColor.mCInk50,
-            borderRadius: BorderRadius.all(Radius.circular(6.0)),
+            borderRadius: BorderRadius.all(
+              Radius.circular(6.0),
+            ),
           ),
           child: Align(
             alignment: Alignment.center,
-            child: TextFormField(
-              // obscureText: _isObscure,
-              // textAlign: TextAlign.center,
-              controller: controller,
-              obscureText: _isObscure,
-              style: TextStyle(color: Colors.red, fontSize: 16.sp),
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                hintText: '• • • • • • • • • • •',
-                hintStyle: TextStyle(fontSize: 16.sp, color: AppColor.mCInk900),
-                contentPadding:
-                    // EdgeInsets.only(left: 12, right: 44, top: 12, bottom: 12),
-                    EdgeInsets.symmetric(horizontal: 12.h),
-                suffixIcon: Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 10.h),
-                  child: SizedBox(
-                    // width: 20.w,
-                    // height: 24.h,
-                    child: Icon(
-                      size: 24.r,
-                      Icons.visibility_outlined,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  width: 304,
+                  height: 44,
+                  child: TextFormField(
+                    controller: controller,
+                    style: TextStyle(color: AppColor.mCInk900, fontSize: 16),
+                    decoration: InputDecoration(
+                      filled: true,
+                      fillColor: AppColor.mCInk50,
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: activeAlert
+                              ? AppColor.mCRed400
+                              : AppColor.mCInk300,
+                        ),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: AppColor.mCBlue300,
+                        ),
+                      ),
+                      hintText: '• • • • • • • • • • •',
+                      hintStyle:
+                          TextStyle(fontSize: 16, color: AppColor.mCInk900),
+                      contentPadding: EdgeInsets.symmetric(horizontal: 12),
+                      suffixIcon: Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 10),
+                        child: IconButton(
+                          iconSize: 24,
+                          icon: Icon(Icons.visibility_outlined),
+                          onPressed: null,
+                        ),
+                      ),
                     ),
                   ),
                 ),
-              ),
+                showAlertInTextField(
+                    activeAlert: activeAlert, messageAlert: messageAlert),
+              ],
             ),
           ),
         ),
@@ -229,68 +237,130 @@ Widget inputTextField(
   );
 }
 
-Widget checkFormatPassword({@required textCheck, @required boolCheck}) {
-  return Container(
-    // width: 304,
-    // height: 24,
-    // color: Colors.pink,
-    child: Align(
-      alignment: Alignment.center,
-      child: Row(
-        // crossAxisAlignment: CrossAxisAlignment.start,
+Widget showAlertInTextField(
+    {required bool activeAlert, required String messageAlert}) {
+  if (activeAlert == false) {
+    return Container();
+  }
+  return Column(
+    children: [
+      const SizedBox(height: 4),
+      Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Align(
-            alignment: Alignment.center,
-            child: Container(
-              // height: 1.h,
-              // width: 1.w,
-              // size
-              child: Transform.scale(
-                scale: 1.r,
-                child: Checkbox(
-                  // splashRadius: 10,
-                  activeColor: AppColor.mCGreen400,
-
-                  shape: RoundedRectangleBorder(
-                      borderRadius:
-                          BorderRadius.circular(10)), // Rounded Checkbox
-                  // checkColor: Colors.red,
-                  value: boolCheck,
-                  onChanged: (bool? value) {},
-                ),
+          Icon(
+            Icons.dangerous,
+            color: AppColor.mCRed400,
+            size: 14,
+          ),
+          const SizedBox(width: 4),
+          Container(
+            width: 284,
+            child: Text(
+              messageAlert,
+              style: TextStyle(
+                fontFamily: mFontRoboto,
+                fontWeight: FontWeight.w400,
+                fontSize: AppDimens.mFontSizeSmall,
+                color: AppColor.mCRed400,
               ),
             ),
           ),
-          SizedBox(width: 4.w),
-          Align(
-            alignment: Alignment.center,
-            child: Text(
-              textCheck,
-              style: TextStyle(
-                  fontFamily: mFontRoboto,
-                  fontWeight: FontWeight.w400,
-                  fontSize: AppDimens.mFontSizeNormal.sp),
-            ),
-          )
         ],
       ),
-    ),
+    ],
   );
 }
 
-Widget buttonLogin({required textTitle, required funcition}) {
+Widget checkFormatPassword({@required textCheck, @required boolCheck}) {
+  return Row(
+    children: [
+      Align(
+        alignment: Alignment.center,
+        child: Container(
+          height: 24,
+          width: 24,
+          child: Checkbox(
+            activeColor: AppColor.mCGreen400,
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10)), // Rounded Checkbox
+            // checkColor: Colors.red,
+            value: boolCheck,
+            onChanged: (bool? value) {},
+          ),
+        ),
+      ),
+      const SizedBox(width: 4),
+      Align(
+        alignment: Alignment.center,
+        child: Text(
+          textCheck,
+          style: TextStyle(
+              fontFamily: mFontRoboto,
+              fontWeight: FontWeight.w400,
+              fontSize: AppDimens.mFontSizeNormal),
+        ),
+      )
+    ],
+  );
+}
+
+Widget buttonChange({required textTitle, required funcition}) {
   return ElevatedButton(
     onPressed: () => {
       funcition,
     },
     child: Text(
       textTitle,
-      style: TextStyle(
+      style: const TextStyle(
           fontFamily: mFontRoboto,
           color: AppColor.mCWhite,
-          fontSize: AppDimens.mFontSizeMedium.sp),
+          fontSize: AppDimens.mFontSizeMedium),
     ),
     style: ElevatedButton.styleFrom(
-        fixedSize: Size(304.w, 44.h), primary: AppColor.mCPrimary400),
+        fixedSize: Size(304, 44), primary: AppColor.mCPrimary400),
+  );
+}
+
+Widget buttonChangeAndCancel(
+    {required textTitleChange,
+    required funcitionChange,
+    required textTitleCancel,
+    required funcitionCancel}) {
+  return Row(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      ElevatedButton(
+        onPressed: () => {
+          funcitionChange,
+        },
+        child: Text(
+          textTitleChange,
+          style: TextStyle(
+              fontFamily: mFontRoboto,
+              color: AppColor.mCWhite,
+              fontSize: AppDimens.mFontSizeMedium),
+        ),
+        style: ElevatedButton.styleFrom(
+            fixedSize: Size(100, 44), primary: AppColor.mCPrimary400),
+      ),
+      const SizedBox(width: 4),
+      ElevatedButton(
+        onPressed: () => {
+          funcitionCancel,
+        },
+        child: Text(
+          textTitleCancel,
+          style: TextStyle(
+              fontFamily: mFontRoboto,
+              color: AppColor.mCInk900,
+              fontSize: AppDimens.mFontSizeMedium),
+        ),
+        style: ElevatedButton.styleFrom(
+          fixedSize: Size(66, 44),
+          primary: AppColor.mCInk300,
+        ),
+      ),
+    ],
   );
 }
